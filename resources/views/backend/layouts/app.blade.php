08401385e0e2d47454ceaddebdd07e12eb5a4c23
phoherozombie <phucdo3687076@gmail.com>
2025-08-29 11:06:01 +0700

    edit

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -340,7 +340,7 @@
         ***********************************-->
         <div class="footer">
             <div class="copyright">
-                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">DexignLab</a> 2020</p>
+                <p>Copyright © Designed &amp; Developed by <a href="../index.htm" target="_blank">HoangPhuc</a> 2025</p>
             </div>
         </div>
         <!--**********************************
